afb15c92313dcd7b10db6a0f016f8d4c244e6b12
waris hayat <warishayat666@gmail.com>
2025-08-29 15:18:30 +0500

    Updates

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
 function Home() {
+  const navigate = useNavigate();
+  const storedUser = localStorage.getItem("user");
+  const user = storedUser ? JSON.parse(storedUser) : {name:" ",email:" "}
+  console.log(storedUser);
+  const handleLogout = () => {
+    localStorage.removeItem("token"); 
+    navigate("/"); 
+  };
   return (
     <div>
-      <h2>Welcome to the home page</h2>
+      <h2>Welcome to the home {user.name}</h2>
+      <button onClick={handleLogout}>Logout</button>
     </div>
-  )
+  );
 }
-
 export default Home
